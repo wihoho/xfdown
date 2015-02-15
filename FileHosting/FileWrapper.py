cb7a93dff4ad68a2cbe942a4e7b612c737832a25
wihoho <wihoho@gmail.com>
2015-02-16 00:10:55 +0800

    add project to perform file hosting

---
+++ FileHosting/FileWrapper.py
@@ -0,0 +1,8 @@
+__author__ = 'GongLi'
+
+
+class FileWrapper:
+
+    def __init__(self, fileName, downloadLink):
+        self.fileName = fileName
+        self.downloadLink = downloadLink
---
+++ FileHosting/LearningFlask.py
@@ -0,0 +1,29 @@
+from flask import Flask, send_from_directory, render_template
+import os
+from FileWrapper import FileWrapper
+
+app = Flask(__name__)
+
+targetFolder = '/Users/GongLi/Github/xfdown/FileHosting/static'
+
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello Wolrd'
+
+@app.route('/list')
+def list():
+
+    allFiles = os.listdir( targetFolder )
+    allFileWrapper = [FileWrapper(address, '/download/' + address) for address in allFiles]
+
+    return render_template('hello.html', files=allFileWrapper)
+
+
+@app.route('/download/<path:filename>')
+def download_file(filename):
+    return send_from_directory(targetFolder, filename, as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run()
